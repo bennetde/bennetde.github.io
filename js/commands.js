1251c5f53eaa02bc891b507e75ee20c8220fafd6
Bennet <bennet.deboben@gmail.com>
2021-04-12 16:16:50 +0200

    Cleanup, some new commands

---
+++ js/commands.js
@@ -0,0 +1,77 @@
+function createText(text) {
+    let node = document.createElement("p");
+    node.innerHTML = text;
+    return node;
+}
+
+function createImage(src) {
+    let node = document.createElement("img");
+    node.src = `svg/${src}.svg`;
+    return node;
+}
+
+function createImageWithText(text, src, textIsLink, link) {
+    let node = document.createElement("div");
+    node.className = 'input';
+
+    node.appendChild(createImage(src));
+    if(textIsLink) {
+        node.appendChild(createLink(" " + text, link));
+    } else {
+        node.appendChild(createText(" " + text));
+    }
+
+    return node;
+}
+
+function createLink(text, link) {
+    let node = document.createElement("a");
+    node.innerText = text;
+    node.href = link;
+    return node;
+}
+
+function echo(text) {
+    return createText(text);
+}
+
+function help() {
+    return createText("All commands:\n" + Object.keys(commands).join("\n"));
+}
+
+function clear() {
+    document.getElementById('previousCommands').innerHTML = '';
+    return document.createElement("div");
+}
+
+function about() {
+    let str = `name: ${data.about.name}\nbirthdate: ${data.about.birthDate}\ncurrently: ${data.about.currently}`
+    return createText(str);
+}
+
+function skills() {
+    let str = `languages: ${data.skills.languages}\nlibraries/engines: ${data.skills.librariesAndEngines}`
+    return createText(str);
+}
+
+function contact() {
+    
+    let divider = document.createElement('div');
+    divider.className = 'commands'
+    let discord = createImageWithText(data.contact.discord, 'discord', false, null)
+    let github = createImageWithText('GitHub', 'github', true, data.contact.github);
+
+    divider.appendChild(github);
+    divider.appendChild(discord);
+    return divider;
+}
+
+
+let commands = {
+    "help": help,
+    "clear": clear,
+    "echo": echo,
+    "about": about,
+    "skills": skills,
+    "contact": contact
+}
---
+++ js/data.js
@@ -0,0 +1,16 @@
+var data = {
+    "motd": "Welcome to my... terminal?\n © 2021 Bennet Deboben",
+    "contact": {
+        "github": "https://github.com/bennetde",
+        "discord": "Bennet#6438"
+    },
+    "about": {
+        "name": "Bennet Deboben",
+        "birthDate": "03-01-2001",
+        "currently": "Computer Science Student at TUM"
+    },
+    "skills": {
+        "languages": ["C#", "C", "Java", "JavaScript", "HLSL/GLSL"],
+        "librariesAndEngines": ["Unity", "Godot", "Vue"]
+    }
+}
